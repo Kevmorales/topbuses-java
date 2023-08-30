d593533eacc5244ffaec911ecaa55d2c76c592b6
Kevin Morales <k@hidalgo.se>
2023-08-30 19:35:08 +0200

    controller busline

--- src/main/java/com/kevin/buslines/controller/BusLineController.java
+++ src/main/java/com/kevin/buslines/controller/BusLineController.java
@@ -1,13 +1,18 @@
 package com.kevin.buslines.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class BusLineController {
-    
-    @GetMapping("/test")
-    public String testEndpoint() {
-        return "Hello from Spring Boot!";
+
+    @GetMapping("/top-bus-lines")
+    public String getTopBusLines() {
+        // placeholder
+        return "Top Bus Lines Data Here";
     }
+
+    // endpoints here ...
 }
