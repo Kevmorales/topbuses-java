bd8df44bdf09936be948f3f75866877342a84b4a
Kevin Morales <k@hidalgo.se>
2023-08-31 09:57:37 +0200

    added api

--- src/main/java/com/kevin/buslines/BusLinesApplication.java
+++ src/main/java/com/kevin/buslines/BusLinesApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class BusLinesApplication {
+
+Dotenv dotenv = Dotenv.load();
+String apiKey = dotenv.get("API_STOPSTWO");
 
     @Bean
     public RestTemplate restTemplate() {
--- src/main/java/com/kevin/buslines/controller/BusLineController.java
+++ src/main/java/com/kevin/buslines/controller/BusLineController.java
@@ -1,18 +1,33 @@
 package com.kevin.buslines.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 public class BusLineController {
 
-    @GetMapping("/top-bus-lines")
-    public String getTopBusLines() {
-        // placeholder
-        return "Top Bus Lines Data Here";
-    }
+    @Autowired
+    private RestTemplate restTemplate;
 
-    // endpoints here ...
+    @Value("#{systemEnvironment['API_STOPSTWO'] ?: 'DEFAULT_VALUE'}")
+    private String API_STOPSTWO;
+
+    private static final String EXTERNAL_API_URL = "https://api.sl.se/api2/LineData.json?model=JourneyPatternPointOnLine&key=%s&DefaultTransportModeCode=bus";
+
+    @GetMapping("/top-bus-lines")
+    public List<Map.Entry<String, Integer>> getTopBusLines() {
+        String stopsUrl = String.format(EXTERNAL_API_URL, API_STOPSTWO);
+        String response = restTemplate.getForObject(stopsUrl, String.class);
+
+        // logiken här
+        //placeholder
+        return new ArrayList<>();
+    }
 }
